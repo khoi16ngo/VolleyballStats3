304dcba9d176d5b4160cc7ed5bec2c9b7f93ce81
Khoi <khoi16ngo@gmail.com>
2025-04-23 23:20:13 -0600

    update project for tailwind and initialize test see if tailwind install properly

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,9 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <title>Livewire Counter</title>
+    @vite('resources/css/app.css')
     @livewireStyles
 </head>
 <body class="flex items-center justify-center h-screen bg-gray-100">
+    <div class="bg-blue-500 text-white p-10">
+        Tailwind is working!
+    </div>
+
     <livewire:counter />
 
     @livewireScripts
