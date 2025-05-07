d2c495c515dd1b3ef3c7c06946cc5fcfd2fabec5
Khoi <khoi16ngo@gmail.com>
2025-05-06 23:04:48 -0600

    remove laravel and livewire stack

--- resources/views/livewire/counter.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="text-center mt-5">
-    <h1 class="text-2xl">Count: {{ $count }}</h1>
-    <button wire:click="increment" class="px-4 py-2 bg-blue-500 text-white rounded mt-2">
-        +
-    </button>
-</div>
