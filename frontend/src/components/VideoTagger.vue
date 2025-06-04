217753b312b8dde6ab9e253a8b553dae23358f98
Khoi <khoi16ngo@gmail.com>
2025-06-03 23:57:09 -0600

    initial set up for vue and django

---
+++ frontend/src/components/VideoTagger.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+      <iframe
+        width="640"
+        height="360"
+        :src="youtubeEmbedUrl"
+        frameborder="0"
+        allowfullscreen
+        ref="video"
+      ></iframe>
+  
+      <div class="space-x-2 mt-2">
+        <button @click="tagAction('spike')">Spike</button>
+        <button @click="tagAction('dig')">Dig</button>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  import axios from 'axios'
+  import { ref } from 'vue'
+  
+  const youtubeEmbedUrl = 'https://www.youtube.com/embed/YOUR_VIDEO_ID'
+  const tagAction = async (type) => {
+    const action = {
+      time_in_video: getCurrentTime(),
+      action_type: type,
+      player: 'Khoi', // later make this dynamic
+    }
+    await axios.post('/api/actions/', action)
+  }
+  
+  const getCurrentTime = () => {
+    // This would be tied to YouTube API if you want real time
+    return Math.random() * 100 // placeholder
+  }
+  </script>
+  
