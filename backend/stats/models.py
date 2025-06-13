26eb868b4a4f843bd12f59820d49792f9bcafbf8
Khoi <khoi16ngo@gmail.com>
2025-06-13 00:07:13 -0600

    remove backend django stack

--- backend/stats/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Action(models.Model):
-    time_in_video = models.FloatField()
-    action_type = models.CharField(max_length=50)
-    player = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
